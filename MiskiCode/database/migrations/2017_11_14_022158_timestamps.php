7d23b5ac426a6874fab80ac6b74aed2ff2f3ea37
jererot <jererot.roy@gmail.com>
2017-11-16 18:03:04 -0500

    Create
    Create

---
+++ MiskiCode/database/migrations/2017_11_14_022158_timestamps.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Timestamps extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*Schema::table('alumno', function (Blueprint $table) {
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });*/
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
